2ec9bc9c3f9fb88d594cf400b671c7ff76a3b0af
Adrian WEAVER <aweaver@student.42.fr>
2023-01-13 20:14:06 +0100

    ADD ITER.HPP:
    This is a working solution for ex01.

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,17 +6,29 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 15:10:29 by aweaver           #+#    #+#             */
-/*   Updated: 2023/01/11 17:52:58 by aweaver          ###   ########.fr       */
+/*   Updated: 2023/01/13 20:12:49 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
+//this version implicitely authorise modification of parameters
 template <typename T>
-void	iter(T* const & tab, unsigned int size, (funct*)(T))
+void	iter(T* const & tab, unsigned int size, void (*funct)(T))
 {
+	for (unsigned int i = 0; i < size; i++)
+	{
+		(*funct)(tab[i]);
+	}
 }
 
+template <typename T>
+void	print(T element)
+{
+	std::cout << element << std::endl;
+}
+
+#endif
 
 #endif
